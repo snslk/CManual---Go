46296f6d591133bf0a2bc24e5d4fc0acf123ce63
snslk <155723346+snslk@users.noreply.github.com>
2024-07-22 10:10:40 +0530

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,8 @@
+drop table users;
+
+create table users (userid varchar(20) primary key , name varchar(20) , email varchar(20) , mobile varchar(10));
+
+insert into users values ('1','snehal','snehal@gmail.com','9988776655');
+insert into users values ('2','sahil','sahil@gmail.com','9988446655');
+insert into users values ('3','deepak','deepak@gmail.com','9933776655');
+insert into users values ('4','kartik','kartik@gmail.com','9922776655');
